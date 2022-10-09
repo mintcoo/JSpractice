d04ee97335d221b33866055add1ba27a3169a0ca
HanSangHyun <xhaktm3344@gmail.com>
2022-10-10 02:09:58 +0900

    1010home

---
+++ momentum/app.js
@@ -0,0 +1,35 @@
+// let a  = 10;
+// const b  = 5;
+
+// const myName = "Han";
+
+// console.log("hello " + myName);
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a - b);
+
+// const amIFat = false;
+
+// const amI = null;
+// console.log(amIFat);
+// console.log(amI);
+
+// const array = ['mon', 'tue'];
+
+// console.log(array[1]);
+
+// array.push('wed');
+// console.log(array);
+
+const player = {
+  name: "Han",
+  points: 100,
+  fat: true,
+};
+
+
+player.points += 10;
+console.log(player.name);
+console.log(player);
