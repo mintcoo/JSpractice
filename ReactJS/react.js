cc6d6cac3622f4d0036a271de87ed4baae5e5a47
HanSangHyun <xhaktm3344@gmail.com>
2022-12-18 23:08:18 +0900

    1218 react JSX

--- ReactJS/react.js
+++ ReactJS/react.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const root = document.querySelector("#root");
-	const h3 = React.createElement(
-		"h3",
-		{
-			onMouseEnter: () => console.log("이것도 되냐??"),
-		},
-		"Hello I'm Span"
-	);
-	const button = React.createElement(
-		"button",
-		{ onClick: () => console.log("눌렀따") },
-		"click me"
-	);
-	const container = React.createElement("div", null, [h3, button]);
+	const title = <h3 id="title" onMouseEnter={() => console.log("mouse enter")}>Hello Im a titoe</h3>
+	// const h3 = React.createElement(
+	// 	"h3",
+	// 	{
+	// 		onMouseEnter: () => console.log("이것도 되냐??"),
+	// 	},
+	// 	"Hello I'm Span"
+	// );
+	const btn = <button onClick={() => console.log("버튼클릭")}>Click Me!!</button>
+	// const button = React.createElement(
+	// 	"button",
+	// 	{ onClick: () => console.log("눌렀따") },
+	// 	"click me"
+	// );
+	const container = React.createElement("div", null, [title, btn]);
 	ReactDOM.render(container, root);
 });
