7db34c0116ac49547e96c7a49958a07426a7f0cb
HanSangHyun <xhaktm3344@gmail.com>
2023-01-01 01:56:37 +0900

    useEffect

--- ReactJS/my-first-app/src/App.js
+++ ReactJS/my-first-app/src/App.js
@@ -1,15 +1,38 @@
-import Button from './Button';
-import styles from './App.module.css'
-import { useState } from 'react';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
 	const [counter, setValue] = useState(0);
-	const onClick = () => setValue((prev) => prev + 1);
+	const [keyword, setKeyword] = useState("");
+	const onChange = event => setKeyword(event.target.value);
+	const onClick = () => setValue(prev => prev + 1);
+	// console.log("alltime");
+	const iRunOneTime = () => {
+		console.log("only oneTime");
+	};
+	useEffect(iRunOneTime, []);
+	useEffect(() => {
+		if (keyword !== "") {
+			console.log("searching changed keyword", keyword);
+		}
+	}, [keyword]);
+		useEffect(() => {
+		if (counter !== "") {
+			console.log("searching changed counter", counter);
+		}
+	}, [counter]);
 	return (
 		<div>
+			<h1>{keyword}</h1>
+			<input
+				value={keyword}
+				onChange={onChange}
+				type="text"
+				placeholder="searchBar"></input>
 			<h1 className={styles.title}>{counter}</h1>
 			<button onClick={onClick}>Click me!!</button>
-			<Button text={"Continue"}/>
+			<Button text={"Continue"} />
 		</div>
 	);
 }
