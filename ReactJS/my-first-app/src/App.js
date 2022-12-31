4a0e7065b295dc4a29f60de827bb38a9ee9bd7f0
HanSangHyun <xhaktm3344@gmail.com>
2022-12-31 23:05:02 +0900

    class module

--- ReactJS/my-first-app/src/App.js
+++ ReactJS/my-first-app/src/App.js
@@ -1,10 +1,14 @@
 import Button from './Button';
 import styles from './App.module.css'
+import { useState } from 'react';
 
 function App() {
+	const [counter, setValue] = useState(0);
+	const onClick = () => setValue((prev) => prev + 1);
 	return (
 		<div>
-			<h1 className={styles.title}>hello world!!</h1>
+			<h1 className={styles.title}>{counter}</h1>
+			<button onClick={onClick}>Click me!!</button>
 			<Button text={"Continue"}/>
 		</div>
 	);
