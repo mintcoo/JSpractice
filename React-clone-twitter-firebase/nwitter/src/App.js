9537f142dabae5258e6537d09d224eaef796f68e
mintcoo <xhaktm3344@gmail.com>
2023-01-10 17:52:41 +0900

    clone twitter start

--- React-clone-twitter-firebase/nwitter/src/App.js
+++ React-clone-twitter-firebase/nwitter/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react"
+import Home from "./routes/Home";
+
 
 function App() {
-  return (
-    <div>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(setIsLoggedIn)
 
-    </div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Home /> : <div>로그인안됨</div>} />
+        
+      </Routes>
+    </Router>
   );
 }
 
--- React-clone-twitter-firebase/nwitter/src/index.js
+++ React-clone-twitter-firebase/nwitter/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// import firebase from "firebase/compat/app";
+import app from './firebase'
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+// import "firebase/compat/storage";
+
+// console.log(firebase);
+console.log(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
