66f542ebb23b31842d2ec51a22a756bbeb74d188
mintcoo <xhaktm3344@gmail.com>
2023-01-11 17:17:35 +0900

    firebase coding

--- React-clone-twitter-firebase/nwitter/src/App.js
+++ React-clone-twitter-firebase/nwitter/src/App.js
@@ -1,13 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { authService } from "./firebase"
+import { onAuthStateChanged } from "firebase/auth";
 import Home from "./routes/Home";
 import Auth from "./routes/Auth";
 
 function App() {
-	console.log('23123',authService.currentUser);
+	// console.log('23123',authService.currentUser);
+	const [init, setInit] = useState(false);
 	const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-	console.log(setIsLoggedIn);
+	useEffect(() => {
+		onAuthStateChanged(authService, (user) => {
+			if (user) {
+				setIsLoggedIn(true);
+				const uid = user.uid;
+			} else {
+				setIsLoggedIn(false);
+			}
+			setInit(true)
+		})
+	}, [])
 
 	return (
 		<Router>
--- React-clone-twitter-firebase/nwitter/src/routes/Auth.js
+++ React-clone-twitter-firebase/nwitter/src/routes/Auth.js
@@ -1,7 +1,76 @@
-import React from "react";
+import { React, useState } from "react";
+import { authService } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Auth = () => {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    console.log(event.target.name);
+    const { name, value } = event.target;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create newAccount
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        // Log in
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log("test", data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Log In"}
+        />
+        <div>
+          <button>Continue with Google</button>
+          <button>Continue with Google</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
