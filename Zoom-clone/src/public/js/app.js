e598797569d70cba11b7e85c938fad626239f30f
mintcoo <xhaktm3344@gmail.com>
2023-01-30 11:15:27 +0900

    socket io start

--- Zoom-clone/src/public/js/app.js
+++ Zoom-clone/src/public/js/app.js
@@ -1,45 +1,68 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-const makeMessage = (type, payload) => {
-	const msg = { type, payload };
-	return JSON.stringify(msg);
-};
 
-socket.addEventListener("open", () => {
-	console.log("서버연결됨 ㅋ");
-});
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-socket.addEventListener("message", message => {
-	// console.log("New message: ", message.data);
-	const li = document.createElement("li");
-	li.innerText = message.data;
-	messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-	console.log("Disconnected from Server ❌");
-});
-
-// setTimeout(() => {
-//   socket.send("hello from the browser!");
-// }, 3000);
-
-function handleSubmit(event) {
-	event.preventDefault();
-	const input = messageForm.querySelector("input");
-	socket.send(makeMessage("new_message", input.value));
-	input.value = "";
+function backendDone(msg) {
+  console.log(`The backend says: `, msg);
 }
 
-function handleNickSubmit(event) {
-	event.preventDefault();
-	const input = nickForm.querySelector("input");
-	socket.send(makeMessage("nickname", input.value));
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+	// 내가 원하는 무슨 이벤트든지 이름정하고 emit으로 보내주면된다 그리고 함수도 보내줄수있다.
+	// 그러면 백엔드에서 함수실행하면 프론트에서 뜬다 !! 그런데 이 함수는 꼭 마지막인자로 넣어야한다
+	// 백엔드에서 바로 먼가 함수실행하는건 위험하니까 프론트를 통해 실행시키는것
+	socket.emit("enter_room", input.value, backendDone);
   input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
+
+// 그냥 webSocket으로만 쓰는 코드
+// const messageList = document.querySelector("ul");
+// const messageForm = document.querySelector("#message");
+// const nickForm = document.querySelector("#nick");
+// const socket = new WebSocket(`ws://${window.location.host}`);
+
+// const makeMessage = (type, payload) => {
+// 	const msg = { type, payload };
+// 	return JSON.stringify(msg);
+// };
+
+// socket.addEventListener("open", () => {
+// 	console.log("서버연결됨 ㅋ");
+// });
+
+// socket.addEventListener("message", message => {
+// 	// console.log("New message: ", message.data);
+// 	const li = document.createElement("li");
+// 	li.innerText = message.data;
+// 	messageList.append(li);
+// });
+
+// socket.addEventListener("close", () => {
+// 	console.log("Disconnected from Server ❌");
+// });
+
+// // setTimeout(() => {
+// //   socket.send("hello from the browser!");
+// // }, 3000);
+
+// function handleSubmit(event) {
+// 	event.preventDefault();
+// 	const input = messageForm.querySelector("input");
+// 	socket.send(makeMessage("new_message", input.value));
+// 	input.value = "";
+// }
+
+// function handleNickSubmit(event) {
+// 	event.preventDefault();
+// 	const input = nickForm.querySelector("input");
+// 	socket.send(makeMessage("nickname", input.value));
+//   input.value = "";
+// }
+
+// messageForm.addEventListener("submit", handleSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
--- Zoom-clone/src/server.js
+++ Zoom-clone/src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import { Server } from "socket.io";
 import express from "express";
 
 const app = express();
@@ -13,42 +14,52 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // app.listen(3000, handleListen);
 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 // 같은 서버에서 http와 webSocket 서버 둘다 작동시키기위해서 뒤에 server를.. 같은포트에서 작동
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
+// 아래껀 websocket 이용
+const wsServer = new Server(httpServer);
 
-// 현재 연결되어있는 브라우저들을 담아주는 리스트
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-	// console.log(socket);
-  socket["nickname"] = "익명";
-	console.log("브라우저랑 연결됨 ㅋ");
-	socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-	socket.on("message", message => {
-		// console.log(message.toString("utf8"));
-    // socket.send(message.toString("utf8"));
-    // 왜이렇게 번거롭게 하냐면 세상에는 JS만 쓰는게 아니라 Object로 보내면안댐 그냥 string로만 보내고 백엔드에서 처리
-    const msg = JSON.parse(message);
-    switch (msg.type) {
-      case "new_message":
-        sockets.forEach((soc) => {
-          // 이건 나한테는 메시지 안보이게할때
-          // if (soc != socket) {
-          //   soc.send(message.toString("utf8"));
-          // }
-          soc.send(`${socket.nickname} : ${msg.payload.toString("utf8")}`);
-        })
-        break
-      case "nickname":
-        // 소켓엔 새로운 아이템추가가능 왜냐면 객체라서 위에 익명처리도해주고
-        socket["nickname"] = msg.payload;
-        console.log(msg.payload)
-        break
-    }
-	});
-	// socket.send("hello!")
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
+    setTimeout(() => {
+      done("hello from the backend");
+    }, 5000);
+  });
 });
+// 현재 연결되어있는 브라우저들을 담아주는 리스트 with websocket
+// const sockets = [];
 
-server.listen(3000, handleListen);
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+// 	// console.log(socket);
+//   socket["nickname"] = "익명";
+// 	console.log("브라우저랑 연결됨 ㅋ");
+// 	socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+// 	socket.on("message", message => {
+// 		// console.log(message.toString("utf8"));
+//     // socket.send(message.toString("utf8"));
+//     // 왜이렇게 번거롭게 하냐면 세상에는 JS만 쓰는게 아니라 Object로 보내면안댐 그냥 string로만 보내고 백엔드에서 처리
+//     const msg = JSON.parse(message);
+//     switch (msg.type) {
+//       case "new_message":
+//         sockets.forEach((soc) => {
+//           // 이건 나한테는 메시지 안보이게할때
+//           // if (soc != socket) {
+//           //   soc.send(message.toString("utf8"));
+//           // }
+//           soc.send(`${socket.nickname} : ${msg.payload.toString("utf8")}`);
+//         })
+//         break
+//       case "nickname":
+//         // 소켓엔 새로운 아이템추가가능 왜냐면 객체라서 위에 익명처리도해주고
+//         socket["nickname"] = msg.payload;
+//         console.log(msg.payload)
+//         break
+//     }
+// 	});
+// 	// socket.send("hello!")
+// });
+
+httpServer.listen(3000, handleListen);
